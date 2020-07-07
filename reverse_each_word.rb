f85021241ed6df39b91d47c51f249d79c447774c
Jackson Reynolds <jwreynold@gmail.com>
2020-07-07 17:55:51 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,2 +1,11 @@
 def reverse_each_word(string)
   str_array = string.split(' ')
+  rev_str_array = []
+  str_array.each do |word|
+    rev_str_array << word.reverse
+  end #each
+  puts rev_str_array
+
+end #reverse_each_word
+
+reverse_each_word("not just one")
